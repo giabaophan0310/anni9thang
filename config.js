ce9ed2334d2c798e95a0c9ba4794eccd254842ca
Nguyễn Phan Gia Bảo <71300593+giabaophan0310@users.noreply.github.com>
2021-07-01 19:42:44 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'He luuu Quân!',
+    introDesc: `bé Tuyết Nhung của anh nè chớp mắt thì em đã lạc vào tim anh cũng đã được 9 tháng òi <3,
+    btnIntro: 'Bắt đầu hoiii',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
+    desc: 'Lần đầu tiên gặp anh em có suy nghĩ gì? :>> ',
+    btnYes: 'Anh là con heooo thúi <3',
+    btnNo: 'Đẹp trai siêu cấp vip pro :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu bé <3',
+    btnReply: 'Gửi cho bé <3',
+    reply: 'Tại bé xinh đẹp tuyệt trần <33333333',
+    mess: 'Cảm ơn anh vì đã đến. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Gặp nhau anh sẽ có quà nha. Còn bây giờ thì thơm em đi nè :>',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/tuyetnhung191999'
+}
